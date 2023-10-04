765cb6e929dd90ade78131241ec70bbbba55b4c5
Kevin Cordoba <kecordoba@ic-itcr.ac.cr>
2023-10-04 01:14:56 -0600

    add react-calendar

--- src/components/Agenda.jsx
+++ src/components/Agenda.jsx
@@ -1,16 +1,26 @@
 import React, { useState } from "react";
 import {useNavigate} from 'react-router-dom'
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
 
 const Agenda = () => {
-    const prueba = useState("");
+    const [date, setDate] = useState(new Date());
 
+    const handleDateChange = (date) => {
+        setDate(date);
+      };
 
     const navigate = useNavigate();
 
     return (
         <div className="agenda-container">
-            <h1 className="title">Agenda</h1>
-        </div>
+
+            <h1>Agenda para {date.toLocaleDateString()}</h1>
+            <div className="calendar-container">
+                <Calendar onChange={handleDateChange} value={date} />
+            </div>
+
+        </div> 
 
     );
 };
