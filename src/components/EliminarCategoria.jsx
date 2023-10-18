9bbf31a0756d14f6082aca8681ecf08730eb7b3a
Kevin Cordoba <kecordoba@ic-itcr.ac.cr>
2023-10-18 05:53:29 -0600

    eliminar categoria

--- src/components/EliminarCategoria.jsx
+++ src/components/EliminarCategoria.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { collection, addDoc, serverTimestamp, getDocs, query, where, onSnapshot } from 'firebase/firestore';
 import { useNavigate } from 'react-router-dom';
+import { doc, deleteDoc } from 'firebase/firestore';
 import { db, storage } from '../firebase/firebaseConfig';
 import 'firebase/firestore';
 
@@ -8,6 +9,7 @@ const EliminarCategoria = () => {
     const [selectedCategory, setSelectedCategory] = useState('');
     const [categories, setCategories] = useState([]);
     const navigate = useNavigate();
+    const [errorText, setErrorText] = useState("");
 
     useEffect(() => {
         const fetchCategories = async () => {
@@ -22,7 +24,43 @@ const EliminarCategoria = () => {
     }, []);
 
     const handleDeleteCategory = async (e) => {
+        e.preventDefault();
+        
+        if (!selectedCategory) {
+            setErrorText('No se ha seleccionado una categoría para eliminar.');
+            return;
+        }
+        
+        try {
 
+            const querySnapshot = await getDocs(collection(db, 'categoria'));
+
+            querySnapshot.forEach(async (doc) => {
+                const data = doc.data();
+                if (data.nombre === selectedCategory) {
+                    const categoryRef = doc.ref;
+
+                    await deleteDoc(categoryRef);
+
+                    console.log(`Categoría "${selectedCategory}" eliminada con éxito.`);
+
+                    const updatedCategories = await fetchCategories();
+                    setCategories(updatedCategories);
+                }
+
+            });
+        
+        } catch (error) {
+            // Manejo de errores: muestra un mensaje de error al usuario o realiza cualquier otra acción necesaria.
+            console.error('Error al eliminar la categoría:', error);
+        }
+    };
+
+    const fetchCategories = async () => {
+        const categoryQuery = query(collection(db, 'categoria'));
+        const categorySnapshot = await getDocs(categoryQuery);
+        const categoryData = categorySnapshot.docs.map((doc) => doc.data().nombre);
+        return categoryData;
     };
 
     return (
