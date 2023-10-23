e1783919e05458329c7fe020d391e8e9d4f34af3
Erick13as <alvaradosolanoerick@gmail.com>
2023-10-22 21:55:16 -0600

    Rutas

--- src/App.js
+++ src/App.js
@@ -6,16 +6,13 @@ import "./components/Design.css"
 import { GaleriaSinLogin, GaleriaAdmin, InfoImagenAdmin, GaleriaCliente, SubirImagen, CrearCategoria, CrearSubcategoria, EliminarCategoria, EliminarSubCategoria, MostrarOpcionesAdmin, InfoImagenCliente, EnviarReferencia } from './controllers/ImageGalleryController';
 import { SignIn, SignUp } from './controllers/UserController';
 
-import Agenda from './components/Agenda';
+// import Agenda from './components/Agenda';
 import AgregarProducto from './components/AgregarProducto';
 import VerMasCliente from './components/VerMasCliente';
 import Carrito from './components/Carrito';
 import { AccederTiendaClienteController } from './controllers/AccederTiendaClienteController';
 import { AccederTiendaAdminController } from './controllers/AccederTiendaAdminController';
-import { Prueba } from './controllers/ImageGalleryController';
 import EditarProductoAdmin from './components/EditarProductoAdmin';
-import "./components/Design.css"
-
 
 const App = () => {
   const [carrito, setCarrito] = useState([]);
@@ -38,7 +35,7 @@ const App = () => {
         <Route path='/AgregarProducto' element={<AgregarProducto />} />
         <Route path='AccederTiendaClienteController/' element={<AccederTiendaClienteController />}/>
         <Route path='AccederTiendaAdminController/' element={<AccederTiendaAdminController />}/>
-        <Route path='/agenda' element={<Agenda />} />
+        {/* <Route path='/agenda' element={<Agenda />} /> */}
         <Route path='/infoImagenCliente' element={<InfoImagenCliente />} />
         <Route path="/Carrito" element={<Carrito carrito={carrito} removeFromCart={removeFromCart} />} />
         <Route path='/VerMasCliente/:id' element={<VerMasCliente />}  />
