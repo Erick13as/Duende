f49b6269efaec763eb2c49d3d0fbd6324e991f3d
Sofiavega12 <sofiavegachavarria@gmail.com>
2023-10-25 16:39:55 -0600

    Update EditarProductoAdmin.jsx

--- src/components/EditarProductoAdmin.jsx
+++ src/components/EditarProductoAdmin.jsx
@@ -150,46 +150,62 @@ function EditarProductoAdmin() {
             </button>
           )}
         </div>
-        <div className="product-info">
-          <h1>Detalles del Producto</h1>
-          {isEditing ? (
-            <div>
+      <div className="product-info">
+        <h1>Detalles del Producto</h1>
+        {isEditing ? (
+          <div>
+            <div className="input-field">
+              <label htmlFor="nombre">Nombre del Producto:</label>
               <input
                 type="text"
                 value={editedProduct.nombre}
                 onChange={(e) => setEditedProduct({ ...editedProduct, nombre: e.target.value })}
               />
+            </div>
+            <div className="input-field">
+              <label htmlFor="precio">Precio:</label>
               <input
                 type="number"
                 value={editedProduct.precio}
                 onChange={(e) => setEditedProduct({ ...editedProduct, precio: parseFloat(e.target.value) })}
               />
+            </div>
+            <div className="input-field">
+              <label htmlFor="descripcion">Descripción:</label>
               <input
                 type="text"
                 value={editedProduct.descripcion}
                 onChange={(e) => setEditedProduct({ ...editedProduct, descripcion: e.target.value })}
               />
+            </div>
+            <div className="input-field">
+              <label htmlFor="cantidad">Cantidad:</label>
               <input
                 type="number"
                 value={editedProduct.cantidad}
                 onChange={(e) => setEditedProduct({ ...editedProduct, cantidad: parseInt(e.target.value) })}
               />
+            </div>
+            <div className="input-field">
+              <label htmlFor="marca">Marca:</label>
               <input
                 type="text"
                 value={editedProduct.marca}
                 onChange={(e) => setEditedProduct({ ...editedProduct, marca: e.target.value })}
               />
             </div>
-          ) : (
-            <div>
-              <h2>Nombre del Producto: {product.nombre}</h2>
-              <p>Precio: {product.precio}</p>
-              <p>Descripción: {product.descripcion}</p>
-              <p>Cantidad: {product.cantidad}</p>
-              <p>Marca: {product.marca}</p>
-            </div>
-          )}
-        </div>
+          </div>
+        ) : (
+          <div>
+            <h2>Nombre del Producto: {product.nombre}</h2>
+            <p>Precio: {product.precio}</p>
+            <p>Descripción: {product.descripcion}</p>
+            <p>Cantidad: {product.cantidad}</p>
+            <p>Marca: {product.marca}</p>
+          </div>
+        )}
+      </div>
+
       </div>
     </div>
   );
