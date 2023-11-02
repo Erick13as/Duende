c309709e67ec66dcf440d3a4dc1acb3453cb81c5
Erick13as <alvaradosolanoerick@gmail.com>
2023-11-01 19:20:24 -0600

    Update Carrito.jsx

--- src/components/Carrito.jsx
+++ src/components/Carrito.jsx
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import {
-  doc,
   updateDoc,
+  deleteDoc, // Importa la función deleteDoc
   collection,
   query,
   where,
   getDocs,
+  doc,
 } from 'firebase/firestore';
 import { db } from '../firebase/firebaseConfig';
 
@@ -55,23 +56,83 @@ function Carrito({ carrito, removeFromCart }) {
   }, [carritoData]);
 
   const handleQuantityChange = async (id, amount) => {
-    if (amount < 1) return;
-
-    const carritoDocRef = doc(db, 'carrito', '1');
-    const newCarritoData = carritoData.map((item) => {
-      if (item.id === id) {
-        item.cantidad = amount;
+    if (amount <= 0) {
+      // Si la cantidad es igual o menor que 0, elimina el producto del carrito y de la colección de Firebase.
+      const updatedCarritoData = carritoData.filter((item) => item.id !== id);
+      setCarritoData(updatedCarritoData);
+  
+      try {
+        const carritoQuery = query(collection(db, 'carrito'), where('correo', '==', email));
+        const carritoQuerySnapshot = await getDocs(carritoQuery);
+  
+        if (carritoQuerySnapshot.size === 0) {
+          console.log("No se encontró un carrito para el correo electrónico proporcionado.");
+          return;
+        }
+  
+        // Supongo que solo hay un documento 'carrito' por correo electrónico, por lo que tomo el primero.
+        const carritoDoc = carritoQuerySnapshot.docs[0];
+  
+        // Elimina el producto del documento 'carrito' en la colección de Firebase.
+        const carritoData = carritoDoc.data();
+        const newCarritoData = carritoData.listaIdCantidadProductos.filter((item) => item.id !== id);
+  
+        // Actualiza el documento 'carrito' en la colección de Firebase sin el producto eliminado.
+        await updateDoc(carritoDoc.ref, {
+          listaIdCantidadProductos: newCarritoData,
+        });
+  
+        // Elimina el producto de la colección 'productos' en Firebase (opcional).
+        // Si deseas mantener un registro de productos eliminados, puedes omitir esta parte.
+        const productRef = doc(collection(db, 'productos'), id);
+        await deleteDoc(productRef);
+      } catch (error) {
+        console.error('Error al eliminar el producto del carrito y la colección de Firebase', error);
       }
-      return item;
-    });
-
-    try {
-      await updateDoc(carritoDocRef, {
-        listaIdCantidadProductos: newCarritoData,
+    } else {
+      // Si la cantidad es mayor que 0, actualiza la cantidad en el carrito.
+      const updatedCarritoData = carritoData.map((item) => {
+        if (item.id === id) {
+          item.cantidad = amount;
+        }
+        return item;
       });
-    } catch (error) {
-      console.error('Error al actualizar la cantidad del producto en el carrito', error);
+      setCarritoData(updatedCarritoData);
+  
+      // Actualiza la cantidad en la lista de productos dentro del documento de carrito en Firebase.
+      try {
+        const carritoQuery = query(collection(db, 'carrito'), where('correo', '==', email));
+        const carritoQuerySnapshot = await getDocs(carritoQuery);
+  
+        if (carritoQuerySnapshot.size === 0) {
+          console.log("No se encontró un carrito para el correo electrónico proporcionado.");
+          return;
+        }
+  
+        // Supongo que solo hay un documento 'carrito' por correo electrónico, por lo que tomo el primero.
+        const carritoDoc = carritoQuerySnapshot.docs[0];
+  
+        // Actualiza el producto en la lista de productos dentro del documento de carrito en la colección de Firebase.
+        const carritoData = carritoDoc.data();
+        const newCarritoData = carritoData.listaIdCantidadProductos.map((item) => {
+          if (item.id === id) {
+            item.cantidad = amount;
+          }
+          return item;
+        });
+  
+        // Actualiza el documento 'carrito' en la colección de Firebase con la cantidad actualizada.
+        await updateDoc(carritoDoc.ref, {
+          listaIdCantidadProductos: newCarritoData,
+        });
+      } catch (error) {
+        console.error('Error al actualizar la cantidad en la colección de Firebase', error);
+      }
     }
+  };
+  
+  const finalizarCompra = () => {
+    navigate('/ingresarDireccion');
   };
 
   useEffect(() => {
@@ -121,10 +182,11 @@ function Carrito({ carrito, removeFromCart }) {
                       -
                     </button>
                     <input
-                      type="number"
+                      type="text"
                       className="carrito-quantity-input"
                       value={item.cantidad}
                       onChange={(e) => handleQuantityChange(item.id, parseInt(e.target.value))}
+                      readOnly={true}
                     />
                     <button
                       onClick={() => handleQuantityChange(item.id, item.cantidad + 1)}
@@ -143,6 +205,9 @@ function Carrito({ carrito, removeFromCart }) {
       <div className="carrito-total" style={{ marginTop: '20px' }}>
         Total: ${total}
       </div>
+      <button onClick={finalizarCompra} className="botonOA">
+      Finalizar Compra
+    </button>
     </div>
   );
 }
