70c5aedb5ccad1319dda549bface8294d15d5734
Kevin Cordoba <kecordoba@ic-itcr.ac.cr>
2023-11-06 21:09:32 -0600

    add prueba evento

--- src/components/Agenda.jsx
+++ src/components/Agenda.jsx
@@ -1,13 +1,36 @@
 import React, { useState } from "react";
-import Fullcalendar from "@fullcalendar/react";
+import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
+import { v4 as uuidv4 } from "uuid";
 
 function Calendar() {
+
+    const [events, setEvents] = useState([]);
+
+    const handleDateSelect = (selectInfo) => {
+        // Crear un nuevo evento y agregarlo al array de eventos
+        const newEvent = {
+            id: uuidv4(),
+            title: "Nuevo Evento",
+            start: selectInfo.startStr,
+            end: selectInfo.endStr,
+        };
+      
+        setEvents([...events, newEvent]);
+      };
+
+    const handleEventClick = (clickInfo) => {
+        // Filtrar los eventos y eliminar el evento seleccionado por su id u otra propiedad única
+        const updatedEvents = events.filter((event) => event.id !== clickInfo.event.id);
+    
+        setEvents(updatedEvents);
+    };
+
     return (
       <div>
-        <Fullcalendar
+        <FullCalendar
           plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
           initialView={"dayGridMonth"}
           headerToolbar={{
@@ -16,6 +39,10 @@ function Calendar() {
             end: "dayGridMonth,timeGridWeek,timeGridDay", // will normally be on the right. if RTL, will be on the left
           }}
           height={"90vh"}
+          selectable={true} // Habilita la selección de fechas
+          select={handleDateSelect}
+          eventClick={handleEventClick}
+          events={events}
         />
       </div>
     );
